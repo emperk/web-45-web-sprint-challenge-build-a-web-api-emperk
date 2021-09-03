b06c692ec568d14245a8e02076a7b962efb56717
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-09-03 13:05:23 -0700

    wrote action get requests

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,26 @@
 // Write your "actions" router here!
+
+const express = require('express');
+const { validateActionId, validateAction } = require('../actions/actions-middlware');
+const Action = require('./actions-model');
+
+const router = express.Router();
+
+// .get()
+
+router.get('/', (req, res, next) => {
+  Action.get()
+    .then(actions => {
+      res.json(actions)
+    })
+    .catch(next)
+})
+
+router.get('/:id', validateActionId, (req, res, next) => {
+  res.json(req.action);
+})
+
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const projectsRouter = require('./projects/projects-router');
+const actionsRouter = require('./actions/actions-router');
 const server = express();
 
 server.use(express.json());
@@ -8,6 +9,7 @@ server.use(express.json());
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
 server.use('/api/projects', projectsRouter);
+server.use('/api/actions', actionsRouter)
 
 server.use('*', (req, res) => {
   res.status(404).json({
