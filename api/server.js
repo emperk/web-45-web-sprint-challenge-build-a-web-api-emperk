4724d6d45b0b687daffe3b383835731c3399eb69
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-09-03 13:14:22 -0700

    complete?

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+
 const projectsRouter = require('./projects/projects-router');
 const actionsRouter = require('./actions/actions-router');
+
 const server = express();
 
 server.use(express.json());
@@ -8,6 +10,7 @@ server.use(express.json());
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
 server.use('/api/projects', projectsRouter);
 server.use('/api/actions', actionsRouter)
 
