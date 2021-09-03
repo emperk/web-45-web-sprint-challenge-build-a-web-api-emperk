85026c04a00681298b4d35b7cb51bd4971d7fc66
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-09-03 11:31:54 -0700

    finished setting project up. working on endpoints now.

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
 // Do NOT `server.listen()` inside this file!
+
+server.use('*', (req, res) => {
+  res.status(404).json({
+    message: '404 Error. Not Found.'
+  })
+})
 
 module.exports = server;
